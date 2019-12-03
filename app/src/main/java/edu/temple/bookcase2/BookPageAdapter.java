2b2c16f0d7d7b07eecadee92d2ed4817314c1acd
JoshuaKang1997 <joshkang1997@gmail.com>
2019-12-02 20:21:15 -0500

    added book page adapter

---
+++ app/src/main/java/edu/temple/bookcase2/BookPageAdapter.java
@@ -0,0 +1,10 @@
+package edu.temple.bookcase2;
+
+import androidx.annotation.NonNull;
+
+import java.util.ArrayList;
+
+public class BookPageAdapter {
+    ArrayList<Book> books;
+    public BookPageAdapter(@NonNull)
+}
